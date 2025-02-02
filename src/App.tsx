57bb44a5f3c48bf56f795c13f354af9a79ae6316
Samarth Kapoor <kapoorsamarth7@gmail.com>
2025-02-02 23:53:47 +0530

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
-import Calendar from "./components/Calendar"
+import React, { useState } from 'react';
+import Calendar from './components/Calendar';
+import { Event, Resource } from './types';
 
 function App() {
-   return (
+  const [events, setEvents] = useState<Event[]>([]);
+  const [resources, setResources] = useState<Resource[]>([
+    { id: '1', name: 'Resource A' },
+    { id: '2', name: 'Resource B' },
+    { id: '3', name: 'Resource C' },
+  ]);
+
+  const handleEventAdd = (event: Event) => {
+    setEvents([...events, event]);
+  };
+
+  const handleEventUpdate = (updatedEvent: Event) => {
+    setEvents(events.map(event => 
+      event.id === updatedEvent.id ? updatedEvent : event
+    ));
+  };
+
+  const handleEventDelete = (eventId: string) => {
+    setEvents(events.filter(event => event.id !== eventId));
+  };
+
+  const handleResourceAdd = (resource: Resource) => {
+    setResources([...resources, resource]);
+  };
+
+  return (
     <div className="h-screen">
-      <Calendar />
+      <Calendar
+        events={events}
+        resources={resources}
+        onEventAdd={handleEventAdd}
+        onEventUpdate={handleEventUpdate}
+        onEventDelete={handleEventDelete}
+        onResourceAdd={handleResourceAdd}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
